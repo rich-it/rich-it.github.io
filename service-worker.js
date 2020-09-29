875bb214e42e1a849a7fc09b9026a6961b2e601a
p32929 <p32929@yahoo.com>
2020-09-29 15:55:34 +0600

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.e810aff71dd68de0fd5613633a25093e.js"
+  "/precache-manifest.9ad0adac8af92d96e4a9e00cba0a7818.js"
 );
 
 self.addEventListener('message', (event) => {
